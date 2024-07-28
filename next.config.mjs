b25432742068d15fe70aaa8594fb1c82076d679f
mmiiprpkr <mmiiprpkr@gmail.com>
2024-07-28 22:00:10 +0700

    add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import withPWA from 'next-pwa';
+
 const nextConfig = {
    images: {
       domains: ["utfs.io"]
-   }
+   },
+   ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+}),
 };
 
 export default nextConfig;
